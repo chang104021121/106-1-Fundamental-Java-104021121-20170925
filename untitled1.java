74a1ac9c9fd7fc9e891ba8c7bf190d050e6b9fbf
chang104021121 <104021121@live.asia.edu.tw>
2017-09-25 16:19:28 +0800

    main

---
+++ untitled1.java
@@ -0,0 +1,8 @@
+    import java.util.Scanner;
+public class untitled1 {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int g=scn.nextInt();
+        System.out.println(Math.round(g*0.00166667f*10)/10f);
+    }
+}
